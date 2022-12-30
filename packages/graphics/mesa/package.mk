b9f7ddf64be9884654ef5fdcba44b928fed067c2
brooksytech <macebrooks@gmail.com>
2022-12-30 13:35:48 -0800

    Update Mesa, Vulkan, & Yuzu

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -11,7 +11,6 @@ PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
-
 get_graphicdrivers
 
 PKG_MESON_OPTS_TARGET="-Ddri-drivers= \
