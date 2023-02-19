47b28bc34e0fe6962697e8bda7091d532c7d95c2
fewtarius <fewtarius@jelos.org>
2023-02-19 06:10:09 -0500

    Fix battery on 503.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -37,7 +37,7 @@ case ${DEVICE} in
   RK3566|RG353P|RG503)
     PKG_SITE="https://github.com/JustEnoughLinuxOS"
     PKG_URL="${PKG_SITE}/rk356x-kernel.git"
-    PKG_VERSION="3cb426d3e"
+    PKG_VERSION="c93baa48c"
     GET_HANDLER_SUPPORT="git"
     PKG_GIT_CLONE_BRANCH="main"
   ;;
