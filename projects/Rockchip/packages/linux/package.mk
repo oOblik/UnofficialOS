1c193904f325a89d1132ececae5fa7e2cd1a53de
fewtarius <fewtarius@jelos.org>
2023-03-08 19:34:43 -0500

    Bump rk3566 kernel.

--- projects/Rockchip/packages/linux/package.mk
+++ projects/Rockchip/packages/linux/package.mk
@@ -37,7 +37,7 @@ case ${DEVICE} in
   RK3566|RG353P|RG503)
     PKG_SITE="https://github.com/JustEnoughLinuxOS"
     PKG_URL="${PKG_SITE}/rk356x-kernel.git"
-    PKG_VERSION="75f9594e2"
+    PKG_VERSION="52be47a8a"
     GET_HANDLER_SUPPORT="git"
     PKG_GIT_CLONE_BRANCH="main"
   ;;
