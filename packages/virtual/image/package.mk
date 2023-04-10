JustEnoughLinuxOS/distribution

Package cleanup.

packages/virtual/image/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="image"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
-PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts network misc-packages debug usb-modeswitch unzip poppler textviewer jq socat p7zip file bluez splash initramfs jelos"
+PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts misc-packages debug usb-modeswitch unzip poppler textviewer jq socat p7zip file bluez splash initramfs jelos"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Root package used to build and create complete image"
 

oOblik/UnofficialOS

packages/virtual/image/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="image"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
-PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts network misc-packages debug usb-modeswitch unzip poppler textviewer jq socat p7zip file bluez splash initramfs unofficialos"
+PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host kmod:host mtools:host populatefs:host libc gcc linux linux-drivers linux-firmware ${BOOTLOADER} busybox util-linux corefonts misc-packages debug usb-modeswitch unzip poppler textviewer jq socat p7zip file bluez splash initramfs unofficialos"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Root package used to build and create complete image"
 